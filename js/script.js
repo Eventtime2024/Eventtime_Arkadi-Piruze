1603072dbd33ca12191f5e1c140d71b34f06bf30
Eventtime2024 <168448599+Eventtime2024@users.noreply.github.com>
2025-03-16 15:18:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-// $(window).on( 'load', function() {
-//     $('html, body').animate({scrollTop : 0});
-//     });
-
+$(window).on( 'load', function() {
+    $('html, body').animate({scrollTop : 0});
+    });
+$(document).on("contextmenu", function(e) {
+    e.preventDefault();
+});
 
 let date=new Date("April 25 2025 00:00:00");
 function counts(){
@@ -20,15 +22,45 @@ function counts(){
  $("#h").text(formatted_hours) ;
   $("#m").text(formatted_minutes);
   $("#s").text(formatted_seconds);
-  if(gap<0){
-    $("#tillwedding").html("<p>Մեր հարսանիքը"+"<br>"+"25.04.2025</p>")
-    $("#group").text(" ")
-  }
-  if(days<2){
-   $(".lastpart").css("display","none")
-  }
+  if (gap <= 0) {
+    $("#d").text("00");
+    $("#h").text("00");
+    $("#m").text("00");
+    $("#s").text("00");
+  } 
+ 
 
 }
 counts();
 setInterval(counts,1000);
 
+function sendMail(e) {
+
+  let params = {
+      choose: document.querySelector('input[name="choose"]:checked').value,
+      guestname: document.getElementById("guestname").value,
+      guestnumber: document.getElementById("guestnumber").value,
+  };
+
+  const serviceID = "sservice_xof245b";
+  const templateID = "template_o30tlw9";
+  emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+          (document.querySelector('input[name="choose"]:checked').value = ""),
+              (document.getElementById("guestname").value = ""),
+              (document.getElementById("guestnumber").value = ""),
+              console.log(res);
+          console.log("Succesfully");
+          window.location.reload();
+      })
+      .catch((err) => console.log(err));
+}
+
+const sendMailbtn = document.querySelector("#sendMail");
+
+sendMailbtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  sendMail();
+ alert("Ուղարկված է։");
+});
